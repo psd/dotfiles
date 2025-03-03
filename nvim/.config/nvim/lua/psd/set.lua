ad21b8142799492d20453d4d4c8f93662bd2d46d
Paul Downey <psd@whatfettle.com>
2025-03-03 14:03:09 +0000

    Stole ThePrimeagen's settings which mostly match my prefs

---
+++ nvim/.config/nvim/lua/psd/set.lua
@@ -0,0 +1,30 @@
+-- indentation
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- backups
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+-- highlight searching
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+-- relative line numbering
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+-- gutter
+vim.opt.scrolloff = 8
+
+-- stop LSP shaking the page
+vim.opt.signcolumn = "yes"
+
+vim.opt.isfname:append("@-@")
